35d12c09c6eb5dfc4aa066d4f9fd09bf84efa6ae
Ben Nelson <benn@ifit.com>
2017-03-10 03:36:38 -0700

    Added blank functions

---
+++ exercises.js
@@ -0,0 +1,17 @@
+//Do not change any of the function names
+
+function getSquareArea(length, width) {
+  //code here
+}
+
+function getTriangleArea(base, height) {
+  //code here
+}
+
+//Do not modify code below this line.
+////--------------------------------
+
+module.exports = {
+  getSquareArea: getSquareArea,
+  getTriangleArea: getTriangleArea
+};
