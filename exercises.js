a894e005a64d4b111f6a191c6ab094830bed3052
Ben Nelson <benn@ifit.com>
2017-03-10 03:31:54 -0700

    Added inital solutions

---
+++ exercises.js
@@ -0,0 +1,15 @@
+function getSquareArea(length, width) {
+  return length * width;
+}
+
+function getTriangleArea(base, height) {
+  return base * height / 2;
+}
+
+//Do not modify code below this line.
+//--------------------------------
+
+module.exports = {
+  getSquareArea: getSquareArea,
+  getTriangleArea: getTriangleArea
+};
