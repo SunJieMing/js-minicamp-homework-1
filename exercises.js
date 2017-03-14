0cc5f1336d7ab9a3292482b663e4683793650835
Ben Nelson <benn@ifit.com>
2017-03-14 14:58:46 -0600

    Fixed problem description

--- exercises.js
+++ exercises.js
@@ -132,7 +132,7 @@ function getCircleArea(radius) {
 }
 
 function getRectangularPrismVolume(length, width, height) {
-  //return the area of the 3D rectangular prism given the length, width, and height
+  //return the volume of the 3D rectangular prism given the length, width, and height
   //code here
 }
 
