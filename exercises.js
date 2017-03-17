03d13eceebc9df45f27117642e8001517778dfea
srimee <junnamma@yahoo.com>
2017-03-17 10:35:10 -0400

    JS mini boot camp homework1 corrected isOdd function

--- exercises.js
+++ exercises.js
@@ -83,7 +83,7 @@ function isEven(num) {
 }
 
 function isOdd(num) {
-  //return true if num is false
+  //return true if num is odd
   //otherwise return false
   //code here
   if (num%2 !== 0) return true;
